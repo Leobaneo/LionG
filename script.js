017d511d839accf29c5007c726e133b375da06f5
Leobaneo <leandrobaneoamasifuen@gmail.com>
2025-07-06 16:32:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('uploadForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Contenido subido correctamente. (Simulado)');
+});
+
+function payWithStripe() {
+  alert("Redirigiendo a Stripe... (Simulado)");
+}
+
+function payWithPaypal() {
+  alert("Redirigiendo a PayPal... (Simulado)");
+}
